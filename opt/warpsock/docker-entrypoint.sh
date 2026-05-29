8e0fa314fd34096b011044c07a665b57d3d0a7ec
Vladimir Avtsenov <vladimir.lsk.cool@gmail.com>
2026-05-29 23:25:11 +0300

    minor fixes

--- opt/warpsock/docker-entrypoint.sh
+++ opt/warpsock/docker-entrypoint.sh
@@ -17,9 +17,28 @@ else
     echo "Skipping generation of \"${WARP_CONFIG}\" file..."
 fi
 
+escape_sed() {
+    printf '%s' "$1" | sed -e 's/[\/&]/\\&/g'
+}
+
+gen_singbox_cfg() {
+    echo "Generating \"${SB_CONFIG_FILE}\" file..."
+    cat "/opt/warpsock/sing-box.json.template" > "${SB_CONFIG_FILE}"
+    sed -i "s|__WG_IFACE_PRIVATE__|$(escape_sed "$WG_IFACE_PRIVATE")|g" "$SB_CONFIG_FILE"
+    sed -i "s|__WG_IFACE_ADDR_V4__|$(escape_sed "$WG_IFACE_ADDR_V4")|g" "$SB_CONFIG_FILE"
+    sed -i "s|__WG_IFACE_ADDR_V6__|$(escape_sed "$WG_IFACE_ADDR_V6")|g" "$SB_CONFIG_FILE"
+    sed -i "s|__WG_PEER_PUBLIC__|$(escape_sed "$WG_PEER_PUBLIC")|g" "$SB_CONFIG_FILE"
+    sed -i "s|__WG_PEER_ENDPOINT__|$(escape_sed "$WG_PEER_ENDPOINT")|g" "$SB_CONFIG_FILE"
+    sed -i "s|__WG_PEER_PORT__|$(escape_sed "$WG_PEER_PORT")|g" "$SB_CONFIG_FILE"
+    sed -i "s|__WG_MTU__|$(escape_sed "$WG_MTU")|g" "$SB_CONFIG_FILE"
+    sed -i "s|__WG_SYSTEM__|$(escape_sed "$WG_SYSTEM")|g" "$SB_CONFIG_FILE"
+    sed -i "s|__SB_LOG_DISABLED__|$(escape_sed "$SB_LOG_DISABLED")|g" "$SB_CONFIG_FILE"
+    sed -i "s|__SB_LOG_LEVEL__|$(escape_sed "$SB_LOG_LEVEL")|g" "$SB_CONFIG_FILE"
+}
+
 SB_CONFIG_FILE="$(mktemp)"
-echo "Generating \"${SB_CONFIG_FILE}\" file..."
-cat "/opt/warpsock/sing-box.json.template" > "${SB_CONFIG_FILE}"
+: "${SB_LOG_DISABLED:=false}"
+: "${SB_LOG_LEVEL:=error}"
 ADDR_ALL=$(grep -m1 '^Address' "$WARP_CONFIG" | cut -d'=' -f2- | tr -d ' ')
 WG_IFACE_PRIVATE=$(grep -m1 '^PrivateKey' "$WARP_CONFIG" | cut -d'=' -f2- | tr -d ' ')
 WG_IFACE_ADDR_V4=$(echo "$ADDR_ALL" | tr ',' '\n' | grep '/32')
@@ -28,18 +47,20 @@ WG_PEER_PUBLIC=$(grep -m1 '^PublicKey' "$WARP_CONFIG" | cut -d'=' -f2- | tr -d '
 WG_PEER_ENDPOINT=$(grep -m1 '^Endpoint' "$WARP_CONFIG" | cut -d'=' -f2- | cut -d':' -f1 | tr -d ' ')
 WG_PEER_PORT=$(grep -m1 '^Endpoint' "$WARP_CONFIG" | cut -d'=' -f2- | cut -d':' -f2- | tr -d ' ')
 WG_MTU=$(grep -m1 '^MTU' "$WARP_CONFIG" | cut -d'=' -f2- | tr -d ' ')
-escape_sed() {
-    printf '%s' "$1" | sed -e 's/[\/&]/\\&/g'
-}
-sed -i "s|__WG_IFACE_PRIVATE__|$(escape_sed "$WG_IFACE_PRIVATE")|g" "$SB_CONFIG_FILE"
-sed -i "s|__WG_IFACE_ADDR_V4__|$(escape_sed "$WG_IFACE_ADDR_V4")|g" "$SB_CONFIG_FILE"
-sed -i "s|__WG_IFACE_ADDR_V6__|$(escape_sed "$WG_IFACE_ADDR_V6")|g" "$SB_CONFIG_FILE"
-sed -i "s|__WG_PEER_PUBLIC__|$(escape_sed "$WG_PEER_PUBLIC")|g" "$SB_CONFIG_FILE"
-sed -i "s|__WG_PEER_ENDPOINT__|$(escape_sed "$WG_PEER_ENDPOINT")|g" "$SB_CONFIG_FILE"
-sed -i "s|__WG_PEER_PORT__|$(escape_sed "$WG_PEER_PORT")|g" "$SB_CONFIG_FILE"
-sed -i "s|__WG_MTU__|$(escape_sed "$WG_MTU")|g" "$SB_CONFIG_FILE"
+: "${WG_SYSTEM:=true}"
 
-cat "$SB_CONFIG_FILE"
+gen_singbox_cfg
 
 echo "Running sing-box..."
-sing-box -c "${SB_CONFIG_FILE}" run || exit $?
+sing-box -c "${SB_CONFIG_FILE}" run
+rc=$?
+
+if [ "$rc" -eq 1 ] && [ "$WG_SYSTEM" = "true" ]; then
+    echo "Fallback with no system WireGuard..."
+    WG_SYSTEM=false
+    gen_singbox_cfg
+
+    sing-box -c "${SB_CONFIG_FILE}" run
+    exit $?
+fi
+exit "$rc"
